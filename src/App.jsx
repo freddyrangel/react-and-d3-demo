e9f9c0f56d314865eb7d49359da701246a19bb84
Swizec Teller <swizec@swizec.com>
2016-05-08 15:08:43 -0700

    Data loading

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+import React, { Component } from 'react';
+import d3 from 'd3';
+
+class App extends Component {
+    state = {data: []}
+
+    componentWillMount() {
+        d3.csv('data/survey_data_part1.csv')
+          .row((d) => {
+              return {
+                  which_role: d['Which one of these roles are you most interested in?'],
+                  already_working: Boolean(Number(d['Are you already working as a software developer?'])),
+                  expect_earn: Number(d['About how much money do you expect to earn per year at your first developer job (in US Dollars)?'])
+              };
+          })
+          .get((error, rows) => {
+              this.setState({data: rows});
+          });
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                <h2>A FreeCodeCamp Survey exploration</h2>
+                <svg width="800" height="600">
+
+                </svg>
+            </div>
+        );
+    }
+}
+
+export default App;
