321c8a8f5cf41298a6cb88395fb3a77ce7336b61
Swizec Teller <swizec@swizec.com>
2016-05-15 14:57:47 -0700

    Barchart flipped over

--- src/components/Barchart/index.jsx
+++ src/components/Barchart/index.jsx
@@ -9,32 +9,51 @@ const Bar = ({ x, y, width, height, name }) => (
 );
 
 class Barchart extends Component {
+    constructor() {
+        super();
+
+        this.xScale = d3.scaleLinear();
+        this.yScale = d3.scaleOrdinal();
+    }
+
+    componentWillMount() {
+        this.updateD3(this.props);
+    }
+
+    componentWillReceiveProps(newProps) {
+        this.updateD3(newProps);
+    }
+
+    updateD3(newProps) {
+        let counts = this.counts = d3.nest()
+                        .key(newProps.value)
+                        .sortKeys(d3.ascending)
+                        .entries(newProps.data);
+
+        let barHeight = (newProps.height-(this.counts.length*3))/this.counts.length;
+
+        this.xScale
+            .domain([0, d3.max(counts,
+                               ({key, values}) => values.length)])
+            .range([0, newProps.width]);
+
+        this.yScale
+            .domain(counts.map(({key, values}) => key))
+            .range(d3.range(0, barHeight*counts.length, barHeight+3));
+    }
+
     render() {
         let transform = `translate(${this.props.x}, ${this.props.y})`;
 
-        let counts = d3.nest()
-                       .key(this.props.value)
-                       .sortKeys(d3.ascending)
-                       .entries(this.props.data);
-
-        let barWidth = (this.props.width-(counts.length*3))/counts.length,
-
-            xScale = d3.scaleOrdinal()
-                       .domain(counts.map(({key, values}) => key))
-                       .range(d3.range(0, barWidth*counts.length, barWidth+3)),
-
-            valScale = d3.scaleLinear()
-                         .domain([0, d3.max(counts,
-                                            ({key, values}) => values.length)])
-                         .range([0, this.props.height]);
+        let barHeight = (this.props.height-(this.counts.length*3))/this.counts.length;
 
         return (
             <g transform={transform}>
-                {counts.map(({ key, values }, i) => (
-                    <Bar x={xScale(i)}
-                         y={-valScale(values.length)}
-                         width={barWidth}
-                         height={valScale(values.length)}
+                {this.counts.map(({ key, values }, i) => (
+                    <Bar x={0}
+                         y={-this.yScale(i)}
+                         width={this.xScale(values.length)}
+                         height={barHeight}
                          name={key}
                          key={`bar-${key}`} />
                  ))}
